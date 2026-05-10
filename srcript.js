95bac20721c4d5ee347ba0ae685042d79f529991
chemuu <chemutdor@gmail.com>
2026-05-10 17:58:33 +0300

    Add surprise button functionality and enhance section animation

---
+++ srcript.js
@@ -0,0 +1,18 @@
+const supriseBtn = document.getElementById("supriseBtn");
+
+surpriseBtn.addEventListener("click",() => {
+
+    alert("Happy Mothers day mom\n\n" + 
+        "Thankyou for your love,care and support.\n" +
+        "You are the best mom in the world😊"
+    );
+});
+
+
+// floating hearts
+
+const heartsContainer = document.querySelector(".hearts");
+function createHeart() {
+    const heart = document.createElement("span");
+    const emojis = 
+}
